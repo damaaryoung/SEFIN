1a493ccebc775e6b64b68a743a72be0d5e0053b4
alan <alangentina95@gmail.com>
2020-10-19 14:46:05 +0700

    cek sertifikat

---
+++ application/views/master/cek_sertifikat/form_create.php
@@ -0,0 +1,47 @@
+<form class="form-create">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h4 class="modal-title">Create Data</h4>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">×</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Quick Example <small>jQuery Validation</small></h3>
+              </div>
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <div class="form-group mb-0">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" name="terms" class="custom-control-input" id="exampleCheck1">
+                      <label class="custom-control-label" for="exampleCheck1">I agree to the <a href="#">terms of service</a>.</label>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer justify-content-between">
+      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      <button type="submit" class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+</form>
+<!-- jquery-validation -->
+<script src="<?= base_url() ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
+<script src="<?= base_url() ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
+<?php $this->view('master/cek_sertifikat/form_create_js.php'); ?>
