c77c7d5d6acea909875a69dc0898e1f143083019
fiyahalfarizi28 <fiyahalfarizi@student.telkomuniversity.ac.id>
2020-10-30 10:36:43 +0700

    Change lending controller fix

--- application/controllers/Dashboard_lending_controller.php
+++ application/controllers/Dashboard_lending_controller.php
@@ -23,7 +23,8 @@ class Dashboard_lending_controller extends CI_Controller
             $firstParameter = $kode_area;
             
 			if ($kode_area == '*') {
-				$chart_title = "KONSOLIDASI";
+                $chart_title = "KONSOLIDASI";
+                $data['chart_title'] = $chart_title;
 			} else {
                 $all_cabang_by_area = $this->db->where('kode_area', $kode_area)->get('view_kode_kantor')->result();
                 $data['all_cabang_by_area'] = $all_cabang_by_area;
@@ -34,12 +35,12 @@ class Dashboard_lending_controller extends CI_Controller
 				$chart_title = "Konsolidasi";
 			} else {
 				$chart_title = $this->db->where('kode_kantor', $kode_kantor)->get('view_kode_kantor')->row()->nama_kantor;
-			}
+            }
+            $data['chart_title'] = $chart_title;
         }        
 
         $Q = "CALL newwebtool.`sp_dashboard_lending`('$firstParameter', $secondParameter, $thirdParameter)";
         $data['lendingData'] = $this->db->query($Q)->result();
-        $data['chart_title'] = $chart_title;
         
         echo json_encode($data);
     }
